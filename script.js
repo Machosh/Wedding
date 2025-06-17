5353d9ccb5d82369aa8ae3deb51ef3d177b0c03a
Machosh <51686757+Machosh@users.noreply.github.com>
2025-06-17 13:56:42 +0900

    현재 모금액 보이지않게 수정

--- script.js
+++ script.js
@@ -180,7 +180,7 @@ function renderItems(data) {
           </div>
           <div class="detail-row">
             <span class="label">현재 모금액:</span>
-            <span class="value">${Number(item.funded).toLocaleString()}만원</span>
+            <span class="value">${item.goal === '-' ? '-' : Number(item.funded).toLocaleString() + '만원'}</span>
           </div>
           <div class="detail-row">
             <span class="label">남은 금액:</span>
